fff1e3e5a703a2363a47c234e0609a2cf5bf8ec4
underwood3mb <underwood3mb@gmail.com>
2016-12-10 14:30:22 -0500

    tweak and update small things

--- app/js/jokes.js
+++ app/js/jokes.js
@@ -39,18 +39,3 @@ function newJoke(){
 	document.getElementById('answerDisplay').style.backgroundColor = "red";
 };
 
-// var moveTitle = function() {
-// 	var title = $('#jokesTitle')
-// 		color = title.css('color') == 'rgb(29, 125, 35)' ? 'rgb(255, 0, 0)' : 'rgb(29, 125, 35)';
-// 	title.css('color', color);
-// 	setTimeout(function() {
-// 		moveTitle();
-// 	}, 400);
-// }
-
-// $(document).ready(function() {
-// 	setTimeout(function() {
-// 		moveTitle();
-// 	}, 800);
-// });
-
